67e53b9db8c7b9811d5e55b028b8f38908bb30e3
Denis Kryga <xumuk39rus@yandex.ru>
2019-11-17 00:28:15 +0300

    Добавил кучу правок для генерации, теперь можно крутить вправо влево

--- Assets/Scripts/Random/RandomMap.cs
+++ Assets/Scripts/Random/RandomMap.cs
@@ -7,6 +7,7 @@ public class RandomMap : MonoBehaviour
 	[Header("Иерархия")]
 	public GameObject globalBackground;
 	public GameObject globalStatic;
+	public GameObject player;
 
 	[Header("Основные коллайдеры")]
 	public GameObject oneBoardUp;
@@ -18,8 +19,10 @@ public class RandomMap : MonoBehaviour
 	public GameObject oneRampDown;
 	public GameObject oneWallLeft;
 	public GameObject oneWallRight;
+
 	[Header("Объекты в мире")]
 	public GameObject barrel;
+	public int chanceBarrel;
 
 	[Header("Коллекция заднего фона")]
 	public GameObject[] background;
@@ -27,6 +30,8 @@ public class RandomMap : MonoBehaviour
 	public int backgroundCount;
 
 	[Header("Коэффицие́нты рандома")]
+	public string ForwardRandomMap;
+
 	public int floor;
 	public int floorDown;
 
@@ -51,16 +56,54 @@ public class RandomMap : MonoBehaviour
 		statics[1] = oneWallRight;
 
 		maxRandomCount = floor + up + down + trap;
-		MapRandom();
+		if (ForwardRandomMap == "right")
+			MapRandom(true);
+		else
+			MapRandom(false);
     }
 
 	public float start_x, start_y;
 	int x, y;
 
-	private void MapRandom()
+	private void MapRandom(bool flag)
 	{
 		x = y = 0;
-		for (x = 0; x < 5; x++)
+		if (flag)
+		{
+			x = -1;
+			FillUnderFloor(-1);
+			SpawnBackgroundRandom();
+			for (int i = 0; i < 2; i++)
+				SpawnWallLeft(-i - 1);
+			y = 3;
+			SpawnBoard(true);
+			for (int i = 0; i < 5; i++)
+			{
+				x--;
+				SpawnFloor();
+				SpawnBackgroundRandom();
+			}
+		}
+		else
+		{
+			x = 1;
+			FillUnderFloor(-1);
+			SpawnBackgroundRandom();
+			for (int i = 0; i < 2; i++)
+				SpawnWallRight(-i - 1);
+			y = 3;
+			SpawnBoard(false);
+			for (int i = 0; i < 5; i++)
+			{
+				x++;
+				SpawnFloor();
+				SpawnBackgroundRandom();
+			}
+		}
+		x = y = 0;
+		int count = flag ? 5 : -5;
+		int increment = flag ? 1 : -1;
+		for (;flag ? x < count : x > count; x += increment)
 		{
 			GameObject obj = Instantiate(oneFloor, new Vector3(x, y, 0), Quaternion.identity);
 			obj.transform.parent = globalStatic.transform;
@@ -68,7 +111,8 @@ public class RandomMap : MonoBehaviour
 			FillUnderFloor(0);
 		}
 
-		for (x = 5; x < 100; x++)
+		count = flag ? 100 : -100;
+		for (; flag ? x < count : x > count; x += increment)
 		{
 			int rand = Random.Range(0, maxRandomCount);
 			if (rand >= 0 && rand < floor)
@@ -78,7 +122,10 @@ public class RandomMap : MonoBehaviour
 				SpawnRapmDown();
 				SpawnBackgroundRandom();
 
-				x++;
+				if (flag)
+					x++;
+				else
+					x--;
 				SpawnFloor();
 			}
 			else if (rand >= floor + up && rand < maxRandomCount - trap)
@@ -86,18 +133,29 @@ public class RandomMap : MonoBehaviour
 				SpawnRampUp();
 				SpawnBackgroundRandom();
 
-				x++;
+				if (flag)
+					x++;
+				else
+					x--;
 				SpawnFloor();
 			}
 			else if (rand >= maxRandomCount - trap && rand < maxRandomCount)
 			{
 				SpawnBoard(true);
+				FillUnderFloor(1);
 				SpawnBackgroundRandom();
-				x++;
+				if (flag)
+					x++;
+				else
+					x--;
 				SpawnTrap();
 				SpawnBackgroundRandom();
-				x++;
+				if (flag)
+					x++;
+				else
+					x--;
 				SpawnBoard(false);
+				FillUnderFloor(1);
 			}
 			SpawnBackgroundRandom();
 		}
@@ -109,6 +167,7 @@ public class RandomMap : MonoBehaviour
 		FillUnderFloor(0);
 		GameObject obj = Instantiate(oneFloor, new Vector3(x, y, 0), Quaternion.identity);
 		obj.transform.parent = globalStatic.transform;
+		SpawnObject(barrel, chanceBarrel);
 	}
 
 	private void SpawnRapmDown()
@@ -175,7 +234,6 @@ public class RandomMap : MonoBehaviour
 			obj.transform.parent = globalStatic.transform;
 			SpawnWallRight(1);
 		}
-		FillUnderFloor(1);
 	}
 
 	#endregion
@@ -183,9 +241,9 @@ public class RandomMap : MonoBehaviour
 	#region Spawn fill under floor Tiles
 	private void FillUnderFloor(int down)
 	{
-		for (int x = 1 + down; x < floorDown + down; x++)
+		for (int i = 1 + down; i < floorDown + down; i++)
 		{
-			GameObject obj = Instantiate(fillFloor[Random.Range(0, fillFloor.Length)], new Vector3(this.x, y - x, 0), Quaternion.identity);
+			GameObject obj = Instantiate(fillFloor[Random.Range(0, fillFloor.Length)], new Vector3(x, y - i, 0), Quaternion.identity);
 			obj.transform.parent = globalStatic.transform;
 		}
 	}
@@ -194,19 +252,52 @@ public class RandomMap : MonoBehaviour
 	#region Spawn Background Tiles
 	private void SpawnBackgroundRandom()
 	{
-		for (int x = backgroundCount / 2 * -1; x < backgroundCount / 2; x++)
+		for (int i = backgroundCount / 2 * -1; i < backgroundCount / 2; i++)
 		{
-			GameObject obj = Instantiate(background[Random.Range(0, background.Length)], new Vector3(this.x, y + x, 0), Quaternion.identity);
+			GameObject obj = Instantiate(background[Random.Range(0, background.Length)], new Vector3(this.x, y + i, 0), Quaternion.identity);
 			obj.transform.parent = globalBackground.transform;
 		}
 	}
 	#endregion
 
+	#region Spawn Objects
+	private void SpawnObject(GameObject obj, int chance)
+	{
+		if (Random.Range(0, 100) < chance)
+		{
+			GameObject newObj = Instantiate(obj, new Vector3(x, y + 1, 0), Quaternion.identity);
+			newObj.transform.parent = globalBackground.transform;
+		}
+	}
+
+	#endregion
+
+	#region Destroy AllMap
+	private void DestroyMap()
+	{
+		for (int i = 0; i < globalStatic.transform.childCount; i++)
+			Destroy(globalStatic.transform.GetChild(i).gameObject);
+		for (int i = 0; i < globalBackground.transform.childCount; i++)
+			Destroy(globalBackground.transform.GetChild(i).gameObject);
+	}
+	#endregion
+
+	[Header("Генерация новой карты")]
+	public bool generateMap;
 	private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.R))
+		if (Input.GetKeyDown(KeyCode.R) || generateMap)
 		{
-			MapRandom();
+			player.transform.position = new Vector3(0, 5, 0);
+			player.GetComponent<Rigidbody2D>().isKinematic = true;
+			player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			DestroyMap();
+			if (ForwardRandomMap == "right")
+				MapRandom(true);
+			else
+				MapRandom(false);
+			player.GetComponent<Rigidbody2D>().isKinematic = false;
+			generateMap = false;
 		}
 	}
 }
