4c64a21772348df0b47283f5c514e8e12fac5e59
Denis Kryga <xumuk39rus@yandex.ru>
2019-11-16 21:41:53 +0300

    добавил тайл мапы пяти уровней, начал писать код на генерацию уровня

--- Assets/Scripts/Random/RandomMap.cs
+++ Assets/Scripts/Random/RandomMap.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RAndomMap : MonoBehaviour
+public class RandomMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public GameObject barrel;
+	public GameObject oneAcidUp;
+	public GameObject oneAcidDown;
+	public GameObject oneFloor;
+	public GameObject oneRampLeft;
+	public GameObject oneRampRight;
+	public GameObject oneWallLeft;
+	public GameObject oneWallRight;
+
+	GameObject[] objects = new GameObject[8];
+
+	void Start()
     {
-        
+		objects[0] = barrel;
+		objects[1] = oneAcidUp;
+		objects[2] = oneAcidDown;
+		objects[3] = oneFloor;
+		objects[4] = oneRampLeft;
+		objects[5] = oneRampRight;
+		objects[6] = oneWallLeft;
+		objects[7] = oneWallRight;
+		MapRandom();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float start_x, start_y;
+
+	private void MapRandom()
+	{
+		for (int x = 0; x < 100; x++)
+		{
+			GameObject obj = Instantiate(objects[Random.Range(0, 6)]);
+			obj.transform.position = new Vector3(x, 0, 0);
+		}
+
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			MapRandom();
+		}
+	}
 }
