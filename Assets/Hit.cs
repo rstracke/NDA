270e25aeed239bfe527c9fc7d42ccf8aab0e2ba5
Victor Baykov <vic.baykov89@gmail.com>
2019-11-17 02:38:32 +0300

    Screw particles on damage added

---
+++ Assets/Hit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    public ParticleSystem hitParticles;
+    public bool hit;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Hitting")
+        {
+            hitParticles.Emit(20);
+        }
+
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hit)
+        {
+            hitParticles.Emit(20);
+            hit = false;
+        }
+    }
+}
