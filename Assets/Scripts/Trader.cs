c8bd0d465a4519caf0f91e993745359c0aedc12f
Denis Kryga <xumuk39rus@yandex.ru>
2019-11-17 19:47:40 +0300

    update

---
+++ Assets/Scripts/Trader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trader : MonoBehaviour
+{
+	bool trade;
+	private void Update()
+	{
+		if (trade)
+		{
+
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.name == "Player")
+			trade = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.name == "Player")
+			trade = false;
+	}
+}
