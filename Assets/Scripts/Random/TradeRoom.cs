3205a18e83fe0391589485a790be47736ab30bec
Denis Kryga <xumuk39rus@yandex.ru>
2019-11-17 13:42:42 +0300

    update trade

--- Assets/Scripts/Random/TradeRoom.cs
+++ Assets/Scripts/Random/TradeRoom.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class TradeRoom : MonoBehaviour
 {
-	public GameObject oneBoardUp;
-	public GameObject oneBoardDown;
-	public GameObject oneAcidUp;
-	public GameObject oneAcidDown;
-	public GameObject oneFloor;
-	public GameObject oneRampUp;
-	public GameObject oneRampDown;
-	public GameObject oneWallLeft;
-	public GameObject oneWallRight;
+	public int start_x, start_y;
+	private int x, y;
+	RandomMap randomMap;
+
+	private void Start()
+	{
+		x = start_x;
+		y = start_y;
+		randomMap = GetComponent<RandomMap>();
+
+		for (int i = 0; i < 100; i++)
+			SpawnFloor();
+	}
+
+	private void SpawnFloor()
+	{
+		GameObject obj = Instantiate(randomMap.oneFloor, new Vector3(x, y, 0), Quaternion.identity);
+		Debug.Log("debug: " + x + ":" + y);
+		obj.transform.parent = randomMap.globalStatic.transform;
+		obj.name = "111111111111111111111111111111";
+		x++;
+	}
+
+	private void SpawnRapmDown(bool flag)
+	{
+		y++;
+		GameObject obj = Instantiate(randomMap.oneRampDown, new Vector3(x, y, 0), Quaternion.identity);
+		if (!flag)
+			obj.transform.eulerAngles = new Vector3(obj.transform.eulerAngles.x, obj.transform.eulerAngles.y, obj.transform.eulerAngles.z - 90);
+		obj.transform.parent = randomMap.globalStatic.transform;
+	}
+
+	private void SpawnRampUp(bool flag)
+	{
+		GameObject obj = Instantiate(randomMap.oneRampUp, new Vector3(x, y, 0), Quaternion.identity);
+		if (!flag)
+			obj.transform.eulerAngles = new Vector3(obj.transform.eulerAngles.x, obj.transform.eulerAngles.y, obj.transform.eulerAngles.z + 90);
+		obj.transform.parent = randomMap.globalStatic.transform;
+		y--;
+	}
 }
