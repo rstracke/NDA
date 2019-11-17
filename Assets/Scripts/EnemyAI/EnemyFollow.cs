681593138ea14607110e042970b804a1271d457d
Denis Kryga <xumuk39rus@yandex.ru>
2019-11-17 16:34:37 +0300

    переместил объекты, добавил свой синглтон

--- Assets/Scripts/EnemyAI/EnemyFollow.cs
+++ Assets/Scripts/EnemyAI/EnemyFollow.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+	float rot_x, rot_y, rot_z;
     void Update()
     {
-        
+		rot_x = transform.eulerAngles.x;
+		rot_y = transform.eulerAngles.y;
+		rot_z = transform.eulerAngles.z;
+		transform.LookAt(LocalPlayer.Singleton.player.transform);
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, rot_z);
     }
 }
