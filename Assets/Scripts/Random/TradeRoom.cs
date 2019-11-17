9587b60b4dd0c05a9b5ab94da9241fffac35b0c2
Denis Kryga <xumuk39rus@yandex.ru>
2019-11-17 14:30:07 +0300

    update trader

--- Assets/Scripts/Random/TradeRoom.cs
+++ Assets/Scripts/Random/TradeRoom.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TradeRoom : MonoBehaviour
 {
+	public GameObject trade;
 	public int start_x, start_y;
 	private int x, y;
 	RandomMap randomMap;
@@ -14,17 +15,23 @@ public class TradeRoom : MonoBehaviour
 		y = start_y;
 		randomMap = GetComponent<RandomMap>();
 
-		for (int i = 0; i < 100; i++)
+		for (int i = 1; i < 10; i++)
+			SpawnWallLeft(i);
+
+		for (int i = 1; i < 10; i++)
 			SpawnFloor();
+		x++;
+
+		for (int i = 1; i < 10; i++)
+			SpawnWallRight(i);
 	}
 
 	private void SpawnFloor()
 	{
-		GameObject obj = Instantiate(randomMap.oneFloor, new Vector3(x, y, 0), Quaternion.identity);
-		Debug.Log("debug: " + x + ":" + y);
-		obj.transform.parent = randomMap.globalStatic.transform;
-		obj.name = "111111111111111111111111111111";
 		x++;
+		GameObject obj = Instantiate(randomMap.oneFloor, new Vector3(x, y, 0), Quaternion.identity);
+		obj.transform.parent = trade.transform;
+		Debug.Log("debug: " + x + ":" + y);
 	}
 
 	private void SpawnRapmDown(bool flag)
@@ -33,7 +40,7 @@ public class TradeRoom : MonoBehaviour
 		GameObject obj = Instantiate(randomMap.oneRampDown, new Vector3(x, y, 0), Quaternion.identity);
 		if (!flag)
 			obj.transform.eulerAngles = new Vector3(obj.transform.eulerAngles.x, obj.transform.eulerAngles.y, obj.transform.eulerAngles.z - 90);
-		obj.transform.parent = randomMap.globalStatic.transform;
+		obj.transform.parent = trade.transform;
 	}
 
 	private void SpawnRampUp(bool flag)
@@ -41,7 +48,19 @@ public class TradeRoom : MonoBehaviour
 		GameObject obj = Instantiate(randomMap.oneRampUp, new Vector3(x, y, 0), Quaternion.identity);
 		if (!flag)
 			obj.transform.eulerAngles = new Vector3(obj.transform.eulerAngles.x, obj.transform.eulerAngles.y, obj.transform.eulerAngles.z + 90);
-		obj.transform.parent = randomMap.globalStatic.transform;
+		obj.transform.parent = trade.transform; 
 		y--;
+	}
+
+	private void SpawnWallLeft(int koof)
+	{
+		GameObject obj = Instantiate(randomMap.oneWallLeft, new Vector3(x, y + koof, 0), Quaternion.identity);
+		obj.transform.parent = trade.transform;
+	}
+
+	private void SpawnWallRight(int koof)
+	{
+		GameObject obj = Instantiate(randomMap.oneWallRight, new Vector3(x, y + koof, 0), Quaternion.identity);
+		obj.transform.parent = trade.transform;
 	}
 }
